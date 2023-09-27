84e25fa3e5cedc703b97836e9aa21631a8fd98cb
dev_gensoft <developer@gensoft.co.th>
2023-09-27 13:09:27 +0700

    fix jwt

--- middleware/verify_input.js
+++ middleware/verify_input.js
@@ -1,11 +1,11 @@
-import Jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken"
 import * as lib from "#libs/Functions"
 import secret_key from '#constants/key'
 
 export const decodedJWT = (req, res, next) => {
     if (lib.checkCompleteArgument(['verify_token', 'app_id'], req.body)) {
         try {
-            Jwt.verify(req.body.verify_token, secret_key[req.body.app_id], (err, result) => {
+            jwt.verify(req.body.verify_token, secret_key[req.body.app_id], (err, result) => {
                 if (err) {
                     console.error(`[${lib.c_time()}][Authorization] Error => Verify token Error`)
                     res.status(500).end(`[${lib.c_time()}][Authorization] Error => Verify token Error`)
