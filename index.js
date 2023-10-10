75e2613c0a06953dae65fa3eca28d611c9ce3e3e
icerayongsi <icerayongsi@gmail.com>
2023-10-10 03:04:23 +0000

    class-dev

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import https from 'https'
 import fs from 'fs'
+import { InitializeRoute } from '#Routes/initialize.route'
 import CIMB__api__v2 from '#API/CIMB/v2/main'
 import KTB__api__v1 from '#API/KTB/v1/main'
 import KBANK__api__v2 from '#API/KBANK/v2/main'
@@ -10,6 +11,39 @@ import { Startup_Config as radis } from '#cache/redis'
 import configs from '#constants/configs'
 import { c_time } from '#libs/Functions'
 
+class Server {
+    constructor() {
+        this.app = express()
+        this.post = process.env.PORT || configs.api_port
+        this.#initializeMiddlewares()
+        this.#initializeRoutes()
+    }
+
+    #initializeMiddlewares() {
+        app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }))
+        app.use(express.json())
+        app.use(express.urlencoded({ extended: true }))
+    }
+
+    #initializeRoutes() {
+        this.app.use('/', InitializeRoute.router);
+    }
+
+    listen() {
+        const options = {
+            key: fs.readFileSync(`${__dirname}/constants/cert/key.pem`),
+            cert: fs.readFileSync(`${__dirname}/constants/cert/thaicoop_co_2023.crt`),
+            rejectUnauthorized: false
+        }
+        const server = https.createServer(options, router)
+        server.listen(PORT, async () => {
+            console.log(`[${c_time()}][API] Server Listening on PORT :`, PORT)
+            await radis()
+            console.log(`[${c_time()}][Mode] : ${process.env.NODE_ENV === 'dev' ? 'Deverlopment' : 'Production'}`)
+        })
+    }
+}
+
 // INIT
 const PORT = process.env.PORT || configs.api_port
 const router = express()
@@ -17,15 +51,15 @@ const __dirname = getDirName(import.meta.url)
 
 router.use(cors())
 //router.use('/KTB/v1',KTB__api__v1)
-router.use('/CIMB/v2',CIMB__api__v2)
-router.use('/KBANK/v2',KBANK__api__v2)
+router.use('/CIMB/v2', CIMB__api__v2)
+router.use('/KBANK/v2', KBANK__api__v2)
 
 router.get('*', (req, res) => {
     res.status(403).end()
     //res.sendFile(`${__dirname}/src/index.html`)
 })
 
-router.post('/getMode' , (req,res) => {
+router.post('/getMode', (req, res) => {
     res.send(process.env.NODE_ENV === 'dev' ? 'Deverlopment' : 'Production')
 })
 
@@ -35,7 +69,7 @@ const options = {
     cert: fs.readFileSync(`${__dirname}/constants/cert/thaicoop_co_2023.crt`),
     rejectUnauthorized: false
 }
-const server = https.createServer(options,router)
+const server = https.createServer(options, router)
 
 server.listen(PORT, async () => {
     console.log(`[${c_time()}][API] Server Listening on PORT :`, PORT)
