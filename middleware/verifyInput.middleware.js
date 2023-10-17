4e2b7757947fbe1d22f129d95ce70b401b305d59
icerayongsi <icerayongsi@gmail.com>
2023-10-17 14:32:58 +0000

    update logger

--- middleware/verifyInput.middleware.js
+++ middleware/verifyInput.middleware.js
@@ -1,25 +1,26 @@
 import jwt from "jsonwebtoken"
 import secret_key from '#constants/key'
 import { checkCompleteArgument, c_time, getLastPathSegment } from "#Utils/utility.func"
+import { logger } from "#Utils/logger"
 
 export const verifyInput = (req, res, next) => {
     if (checkCompleteArgument(['verify_token', 'app_id'], req.body)) {
         try {
-            jwt.verify(req.body.verify_token, secret_key[req.body.app_id], (err, result) => {
-                if (err) {
-                    console.error(`[${c_time()}][Authorization] Error => Verify token Error`)
+            jwt.verify(req.body.verify_token, secret_key[req.body.app_id], (error, result) => {
+                if (error) {
+                    logger.error(`(JWT) ${error}`)
                     res.status(500).end(`[${c_time()}][Authorization] Error => Verify token Error`)
                 }
                 req.body = result
                 req.body.routePath = getLastPathSegment(req.route.path) 
                 next()
             })
-        } catch (e) {
-            console.error(`[${c_time()}][Unthorization] Error => Verify token Error`)
+        } catch (error) {
+            logger.error(`(JWT) ${error}`)
             res.status(500).end(`[${c_time()}][Unthorization] Error => Verify token Error`)
         }
     } else {
-        console.error(`[${c_time()}][Unthorization] Error => Payload not compalte`)
+        logger.error('Payload not compalte')
         res.status(500).end(`[${c_time()}][Unthorization] Error => Payload not compalte`)
     }
 }
--- utils/logger.js
+++ utils/logger.js
@@ -13,45 +13,45 @@ if (!existsSync(logDir)) mkdirSync(logDir)
 const logFormat = winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`)
 
 const logger = winston.createLogger({
-    format: winston.format.combine(
-      winston.format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss',
-      }),
-      logFormat,
-    ),
-    transports: [
-      new winstonDaily({
-        level: 'debug',
-        datePattern: 'YYYY-MM-DD',
-        dirname: logDir + '/debug',
-        filename: `%DATE%.log`,
-        maxFiles: 30,
-        json: false,
-        zippedArchive: true,
-      }),
-      new winstonDaily({
-        level: 'error',
-        datePattern: 'YYYY-MM-DD',
-        dirname: logDir + '/error',
-        filename: `%DATE%.log`,
-        maxFiles: 30,
-        handleExceptions: true,
-        json: false,
-        zippedArchive: true,
-      }),
-    ],
-  })
-
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.combine(winston.format.splat(), winston.format.colorize()),
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
     }),
-  )
-  
-  const stream = {
-    write : (message) => {
-      logger.info(message.substring(0, message.lastIndexOf('\n')));
-    },
-  }
-  
-  export { logger, stream }
+    logFormat,
+  ),
+  transports: [
+    new winstonDaily({
+      level: 'debug',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir + '/debug',
+      filename: `%DATE%.log`,
+      maxFiles: 30,
+      json: false,
+      zippedArchive: true,
+    }),
+    new winstonDaily({
+      level: 'error',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir + '/error',
+      filename: `%DATE%.log`,
+      maxFiles: 30,
+      handleExceptions: true,
+      json: false,
+      zippedArchive: true,
+    }),
+  ],
+})
+
+logger.add(
+  new winston.transports.Console({
+    format: winston.format.combine(winston.format.splat(), winston.format.colorize()),
+  }),
+)
+
+const stream = {
+  write: (message) => {
+    logger.info(message.substring(0, message.lastIndexOf('\n')));
+  },
+}
+
+export { logger, stream }
