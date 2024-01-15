0f409d6d108dab5dc964ac03582f09836ec221dd
dev_gensoft <developer@gensoft.co.th>
2024-01-15 11:01:51 +0700

    modified:   middleware/auth.middleware.js 	modified:   utils/kbank.func.js

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -15,7 +15,8 @@ export const oAuthV2KBNAK = async (req, res, next) => {
     const credential = `${kbank.constants.credentials[mode].consumer_id}:${kbank.constants.credentials[mode].consumer_secret}`
 
     try {
-        if (await session.getStatus(req.body.unique_key, bankNameInit)) {
+        const checkDupSession = await session.getStatus(req.body.unique_key, bankNameInit)
+        if (checkDupSession) {
             const obj = {
                 headers: {
                     "Authorization": `Basic ${kbank.Base64Encoded(credential)}`,
@@ -26,6 +27,7 @@ export const oAuthV2KBNAK = async (req, res, next) => {
                 }
             }
             const result = await RequestFunction.post(true, endpoint.default.kbank[mode].oAuthV2, obj.headers, obj.body, { ssl: kbank.httpsAgent })
+            console.log(result)
             await session.set(req.body.unique_key, bankNameInit, result.data)
         }
         next()
